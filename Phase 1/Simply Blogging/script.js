1776e50defbbd5ebb12dc1b738f89ed41256ebb8
Phu Nguyen <phulnguyen7@gmail.com>
2021-08-05 01:37:23 -0500

    can add new blogs on top, not push to the right and down

--- Phase 1/Simply Blogging/script.js
+++ Phase 1/Simply Blogging/script.js
@@ -1,1 +1,11 @@
 // 2058499 Simply Blogging
+
+function addBlog() {
+    let title = document.getElementById("title").value;
+    let article = document.getElementById("article").value;
+    let img_url = document.getElementById("img_url").value;
+    let newBlog = `<div><h3>${title}</h3><p>    ${article}</p> <img src="${img_url}"></div>`;
+    let blogs_area = document.getElementById("blogs_area");
+
+    blogs_area.innerHTML = newBlog + blogs_area.innerHTML;
+}
