0aff47e5eb971fd072002ba8b0f79200673c1a98
Phu Nguyen <phulnguyen7@gmail.com>
2021-08-05 14:18:19 -0500

    Simply Blogging v1.0

--- Phase 1/Simply Blogging/script.js
+++ Phase 1/Simply Blogging/script.js
@@ -4,8 +4,13 @@ function addBlog() {
     let title = document.getElementById("title").value;
     let article = document.getElementById("article").value;
     let img_url = document.getElementById("img_url").value;
-    let newBlog = `<div><h3>${title}</h3><p>    ${article}</p> <img src="${img_url}"></div>`;
     let blogs_area = document.getElementById("blogs_area");
+    let newBlog =  
+        `<div class="col-3 article_div">
+            <h3>${title}</h3>
+            <p>${article}</p>
+            <img src="${img_url}" class="article_img">
+        </div>`;
 
     blogs_area.innerHTML = newBlog + blogs_area.innerHTML;
 }
