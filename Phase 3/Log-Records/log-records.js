b0bbd9b6c4dda272873fb13f5648216e1025785c
Phu Nguyen <phulnguyen7@gmail.com>
2021-08-23 15:27:12 -0500

    end. Log Records v1.0

--- Phase 3/Log-Records/log-records.js
+++ Phase 3/Log-Records/log-records.js
@@ -1,1 +1,30 @@
-let readlineSync = require("readline-sync");
+function logRecords() {
+    let fs = require("fs");
+    let readlineSync = require("readline-sync");
+    let records = JSON.parse(fs.readFileSync("./logs.json"));
+    let moreRecord = "n";
+
+    do {
+        let firstname = readlineSync.question("What is your name? ");
+        let lastname = readlineSync.question("What is your lastname? ");
+        let gender = readlineSync.question("What is your gender? ");
+        let email = readlineSync.question("What is your email? ");
+        let datetime = Date();
+
+        records.push({
+            firstname,
+            lastname,
+            gender,
+            email,
+            datetime
+        })
+
+        moreRecord = readlineSync.question("More record? (y/n) ");
+        
+    } while (moreRecord == "y");
+
+    fs.writeFileSync("./logs.json", JSON.stringify(records));
+    console.log("Record saved!");
+}
+
+logRecords();
