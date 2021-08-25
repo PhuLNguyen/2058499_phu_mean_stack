96159d73ee45315c68d6ff1a5094de5085ada805
Phu Nguyen <phulnguyen7@gmail.com>
2021-08-25 16:47:07 -0500

    end. Task Planner v1.0

--- Phase 3/Task-Planner/task-planner.js
+++ Phase 3/Task-Planner/task-planner.js
@@ -2,7 +2,7 @@
 
 let http = require("http");
 let fs = require("fs");
-let tasks = JSON.parse(fs.readFileSync("./tasks.json").toString());
+let url = require("url");
 let PORT = 9090;
 
 let indexPage = `
@@ -17,22 +17,22 @@ let indexPage = `
     <body>
         <h1>Task Planner</h1>
         <h2>Add Task</h2>
-        <form target="addTask">
+        <form action="addTask">
             <label for="empId">Employee ID: </label>
-            <input type="text" name="empId">
+            <input type="text" name="empId"><br>
             <label for="taskId">Task ID: </label>
-            <input type="text" name="taskId">
+            <input type="text" name="taskId"><br>
             <label for="task">Task: </label>
-            <input type="text" name="task">
-            <label for="deadline">Deadline</label>
-            <input type="text" name="deadline">
+            <input type="text" name="task"><br>
+            <label for="deadline">Deadline: </label>
+            <input type="date" name="deadline"><br>
             <input type="submit" value="Add Task">
         </form>
         <br><hr>
         <h2>Delete Task</h2>
         <form action="deleteTask">
-            <label for="delTask">Task ID</label>
-            <input type="text" name="delTask">
+            <label for="delTaskId">Task ID: </label>
+            <input type="text" name="delTaskId"><br>
             <input type="submit" value="Delete Task">
         </form>
         <br><hr>
@@ -44,7 +44,80 @@ let indexPage = `
     </html>
 `;
 
+let tableHeader = `
+    <table border=1>
+        <tr>
+            <th>Employee ID</th>
+            <th>Task ID</th>
+            <th>Task</th>
+            <th>Deadline</th>
+        </tr>
+`;
+
+let tableFooter = `
+    </table>
+`;
+
 let server = http.createServer((request, response) => {
+    let urlInfo = url.parse(request.url, true);
+
+    if (urlInfo.path != "favicon.ico"){
+        let tasks = JSON.parse(fs.readFileSync("./tasks.json").toString());
+        let urlQuery = urlInfo.query;
+
+        response.writeHead(200, {"content-type":"text/html"});
+
+        if (urlInfo.pathname == "/addTask"){
+            let taskIdExist = tasks.find(task => task.taskId == urlQuery.taskId);
+
+            // undefined = not existed
+            if (taskIdExist == undefined){
+                tasks.push(urlQuery);
+                fs.writeFileSync("./tasks.json", JSON.stringify(tasks));
+                response.write(`<p style="color:green">Task ID ${urlQuery.taskId} added</p>`);
+            } else {
+                response.write(`<p style="color:red">Error: Task ID ${urlQuery.taskId} existed</p>`);
+            }
+
+            response.write(indexPage);
+        } else if (urlInfo.pathname == "/deleteTask"){
+            let taskIndex = tasks.findIndex(task => task.taskId == urlQuery.delTaskId);
+            // -1 = not found
+            if (taskIndex == -1){
+                response.write(`<p style="color:red">Error: Task ID ${urlQuery.delTaskId} does not existed</p>`);
+            } else {
+                tasks.splice(taskIndex, 1); // delete 1 element starting from index=taskIndex 
+                fs.writeFileSync("./tasks.json", JSON.stringify(tasks));
+                response.write(`<p style="color:green">Task ID ${urlQuery.delTaskId} deleted</p>`);
+            }
+
+            response.write(indexPage);
+        } else if (urlInfo.pathname == "/listTasks") {
+            let tasksTable = tableHeader;
+
+            tasks.forEach(task => {
+                tasksTable += `
+                    <tr>
+                        <td>${task.empId}</td>
+                        <td>${task.taskId}</td>
+                        <td>${task.task}</td>
+                        <td>${task.deadline}</td>
+                    </tr>
+                `;
+            });
+
+            tasksTable += tableFooter;
+
+            response.write(indexPage);
+            response.write(tasksTable);
+        } else {
+            response.write(indexPage);
+        }
+    }
+
     response.end();
 });
-server.listen(PORT, () => console.log("Server is running on port " + PORT));
+
+server.listen(PORT, () => {
+    console.log("Server is running on localhost:" + PORT);
+});
